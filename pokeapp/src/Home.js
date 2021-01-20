2822eb6e6e6266b5ac10a1ab186a2d3f39b67988
Ana Freitas <analuizac.freitas@gmail.com>
2021-01-20 17:46:16 -0300

    API fetch first version

--- pokeapp/src/Home.js
+++ pokeapp/src/Home.js
@@ -1,12 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import Pokemon from "./Pokemon";
 
 export default function Home() {
+  //API content code below
+  const APP_URL = "https://pokeapi.co/api/v2/pokemon?limit=2000";
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemonsList();
+  }, []);
+
+  const getPokemonsList = async () => {
+    const response = await fetch(APP_URL);
+    const data = await response.json();
+    setPokemons(data.results);
+  };
+  //End API content code
+
   const [pesquisa, setPesquisa] = useState('');
   return (
     <>
       <Header />
       <div className="container mt-4">Home</div>
+      <div className="pokemon-cards">
+        {pokemons.map(item => (
+          <Pokemon
+            key={item.name}
+            name={item.name} 
+            url={item.url} 
+            />
+        ))}
+      </div>
     </>
   );
 }
