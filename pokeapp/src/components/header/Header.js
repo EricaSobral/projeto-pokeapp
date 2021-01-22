b4a5d359c3b9f1a62ef376084aed1535e1cbc785
ericasobral <erica.sobral95@gmail.com>
2021-01-22 12:22:38 -0300

    Enviando a estruturação das páginas do pokeaap

---
+++ pokeapp/src/components/header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+export default function Header() {
+  return (
+    <>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container className="d-flex justify-content-around align-center py-2">
+          <Link to="/">
+            <Navbar.Brand>PokeApp</Navbar.Brand>
+          </Link>
+          <div>
+            <Link to="/">
+              <Navbar.Brand>Home</Navbar.Brand>
+            </Link>
+            <Link to="/meuspokemons">
+              <Navbar.Brand>Meus pokemons</Navbar.Brand>
+            </Link>
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
---
+++ pokeapp/src/pages/home/Home.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/header/Header';
+import Pokemon from "../../components/pokemons/Pokemon";
+import Pagination from '@material-ui/lab/Pagination';
+import LoadingScreen from '../../components/loading/Loading';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+
+import Footer from '../../components/footer/Footer';
+
+export default function Home() {
+  
+  const APP_URL = "https://pokeapi.co/api/v2/pokemon";
+  const LIMIT_ITENS_PER_PAGE = 20;
+  const [ totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true)
+
+  const handleChangePage = (event, value) => {
+    let rangeSelected = ((value-1) <= 0) ? 0 : value - 1;
+    let offset = rangeSelected * LIMIT_ITENS_PER_PAGE;
+    let url_link = APP_URL + `?offset=${ offset }&limite=${LIMIT_ITENS_PER_PAGE}`;
+ 
+    getPokemonsList(url_link);
+  };
+  
+  const [pokemons, setPokemons] = useState([]);
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  useEffect( () => {
+    getPokemonsList('');
+    setTimeout(() => setLoading(false), 1);
+  }, [] );
+
+  const getPokemonsList = async (url) => {
+    let urlSelected = ((url) && (url.length > 0)) ? url : APP_URL;
+    const response = await fetch(urlSelected);
+    const data = await response.json();
+
+    let numberOfPages = Math.ceil(data.count/LIMIT_ITENS_PER_PAGE);  
+    setTotalPages(numberOfPages);
+
+    getAllPokemonsList(data.count);
+
+    setPokemons(data.results);
+  };
+
+  const getAllPokemonsList = async (max) => {
+    let limite = (max) ? max : 5000;
+    let urlLinkAllPokemons = APP_URL + `?limite=${limite}`;
+
+    const response = await fetch(urlLinkAllPokemons);
+    const data = await response.json();
+
+    setAllPokemons(data.results);
+  };
+
+
+  // funcao de filtro dos Pokemons
+  const [pesquisar, setPesquisar] = useState('');
+  const handleChange = (event) => {
+    setPesquisar(event.target.value);
+  };
+
+  useEffect(() => {
+    const filteredPokemons = allPokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(pesquisar)
+    );
+    setPokemons(filteredPokemons);
+  }, [pesquisar]);
+
+
+  return (
+    <>
+      <Header />
+      <div className="container mt-4 d-flex justify-content-between">
+        <h2>Home</h2>
+        <Form className="d-flex mt-2">
+          <FormControl
+            type="text"
+            name="pesquisarPokemon"
+            placeholder="Pesquisar Pokemon"
+            className="mr-sm-2"
+            value={pesquisar}
+            onChange={handleChange}
+          />
+          <Button variant="outline-info">Pesquisar</Button>
+        </Form>
+      </div>
+      {loading === false ? (
+      <div className="pokemon-cards d-flex flex-wrap">
+        {pokemons.map(item => (
+          <Pokemon
+            key={item.name}
+            name={item.name} 
+            url={item.url} 
+            />
+        ))}
+      </div>
+      ) : (
+      <LoadingScreen />
+      )}
+      <Pagination 
+        count={ (totalPages) ? totalPages : 1 } 
+        variant="outlined" 
+        color="secondary" 
+        size="small" 
+        shape="rounded" 
+        showFirstButton 
+        showLastButton
+        onChange={ handleChangePage }  />
+      <Footer />
+    </>
+  );
+}
